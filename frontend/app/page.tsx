e81b9fa53cee8a292724ce37cbf87a59d552c048
christophenne <christoph.enne@glasskube.eu>
2025-02-24 09:51:16 +0100

    feat: show version
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,7 @@ import { Snippet } from "@heroui/snippet";
 import { Code } from "@heroui/code";
 import { button as buttonStyles } from "@heroui/theme";
 
+import { buildConfig } from "@/buildconfig";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
@@ -43,7 +44,8 @@ export default function Home() {
       <div className="mt-8">
         <Snippet hideCopyButton hideSymbol variant="bordered">
           <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
+            hello-distr Version:{" "}
+            <Code color="primary">{buildConfig.version}</Code>
           </span>
         </Snippet>
       </div>
